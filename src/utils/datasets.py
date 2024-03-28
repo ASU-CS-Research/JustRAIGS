2f6aeecb74f6ec1bbc73c757071b0a7f58f8836f
Jake Jenkins <jenkinsjs2@appstate.edu>
2024-03-28 11:10:48 -0400

    Added multiclass labels to Tensorflow Datasets
    - Added a paramater to load_datasets function specifying binary labels or multiclass labels.
    - Labels are encoded in a multihot format.
    - The Grader 1 multiclass labels are used
    	- Using the labels where G1 and G2 agree results in aprox. 200
    	lables for the entire dataset.
    	- The G3 labels are only present when the first 2 Graders don't agree on the final
    	label (NRG vs RG).

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -47,7 +47,8 @@ def load_and_preprocess_image(*args, **kwargs) -> Tuple[np.ndarray, int]:
 def load_datasets(
         color_mode: str, target_size: Optional[Tuple[int, int]], interpolation: str, keep_aspect_ratio: bool,
         num_partitions: int, batch_size: int, num_images: Optional[int] = None, train_set_size: Optional[float] = 0.6,
-        val_set_size: Optional[float] = 0.2, test_set_size: Optional[float] = 0.2, seed: Optional[int] = 42) -> [Dataset, Dataset, Dataset]:
+        val_set_size: Optional[float] = 0.2, test_set_size: Optional[float] = 0.2, seed: Optional[int] = 42,
+        is_multi: Optional[bool] = False) -> [Dataset, Dataset, Dataset]:
     """
     Constructs TensorFlow train, val, test :class:`~tensorflow.data.Dataset` s from the provided high resolution image
     training set.
@@ -79,7 +80,8 @@ def load_datasets(
           to ``0.2`` (e.g. ``20%``).
         seed (Optional[int]): The random seed used to shuffle the data and split the dataset into training, validation,
           and testing sets. This value defaults to ``42``.
-
+        is_multiclass (Optinal[bool]): Flag used to determine if dataset is for binary classification or multiclass.
+          This defaults to ``false``.
     Returns:
         Tuple[:class:`~tensorflow.data.Dataset`, :class:`~tensorflow.data.Dataset`, :class:`~tensorflow.data.Dataset`]:
           A tuple containing the training, validation, and testing datasets.
@@ -111,9 +113,20 @@ def load_datasets(
     # Modify the DataFrame to contain the absolute path to the 'Eye ID' training image:
     train_data_labels_df = train_data_labels_df.merge(image_paths_df, on=['Eye ID'], how='outer')
     del image_paths_df
-    # Convert 'NRG' = 0 and 'RG' = 1
-    final_label_int_mask = train_data_labels_df['Final Label'] == 'NRG'
-    train_data_labels_df['Final Label Int'] = np.where(final_label_int_mask, 0, 1)
+    if(not is_multi):
+        # Convert 'NRG' = 0 and 'RG' = 1
+        final_label_int_mask = train_data_labels_df['Final Label'] == 'NRG'
+        train_data_labels_df['Class Label'] = np.where(final_label_int_mask, 0, 1)
+    else:
+        train_data_labels_df = train_data_labels_df[train_data_labels_df['Final Label'] == "RG"]
+        # Filtering by both graders agreeing on all of the additional labels signifigantly reduces the size
+        # of the dataset to aprox. 186. Additionaly, G3 only grades when G1 and G2 dissagree on the final label
+        # (NRG vs RG). 
+        # train_data_labels_df["multihotG1"] = train_data_labels_df.filter(regex="G1 .*").to_numpy().tolist()
+        # train_data_labels_df["multihotG2"] = train_data_labels_df.filter(regex="G2 .*").to_numpy().tolist()
+        # train_data_labels_df = train_data_labels_df.query("multihotG1 == multihotG2")
+        train_data_labels_df["multihotG1"] = train_data_labels_df.filter(regex="G1 .*").to_numpy().tolist()
+        train_data_labels_df = train_data_labels_df.rename(columns={"multihotG1": "Class Label"})
     # Drop rows with NaN absolute file paths:
     train_data_labels_df = train_data_labels_df[train_data_labels_df['AbsPath'].notna()]
     # Train, Validation, and Testing set partitioning:
@@ -131,37 +144,37 @@ def load_datasets(
     At this point we have the dataframes partitioned into train, validation, and testing sets.
     Now we need to convert them to TensorFlow Datasets:
     '''
-    train_img_and_labels_df = train_ds_df[['AbsPath', 'Final Label Int']].apply(
+    train_img_and_labels_df = train_ds_df[['AbsPath', 'Class Label']].apply(
         load_and_preprocess_image, axis=1, raw=True, result_type='reduce', args=(
             ('color_mode', color_mode), ('target_size', target_size), ('interpolation', interpolation),
             ('keep_aspect_ratio', keep_aspect_ratio)
         )
     )
-    train_img_and_labels_df.rename(columns={'AbsPath': 'NpImage', 'Final Label Int': 'LabelTensor'}, inplace=True)
+    train_img_and_labels_df.rename(columns={'AbsPath': 'NpImage', 'Class Label': 'LabelTensor'}, inplace=True)
     train_ds = tf.data.Dataset.from_tensor_slices(
         (list(train_img_and_labels_df['NpImage']), list(train_img_and_labels_df['LabelTensor']))
     )
     del train_ds_df
     del train_img_and_labels_df
-    val_img_and_labels_df = val_ds_df[['AbsPath', 'Final Label Int']].apply(
+    val_img_and_labels_df = val_ds_df[['AbsPath', 'Class Label']].apply(
         load_and_preprocess_image, axis=1, raw=True, result_type='reduce', args=(
             ('color_mode', color_mode), ('target_size', target_size), ('interpolation', interpolation),
             ('keep_aspect_ratio', keep_aspect_ratio)
         )
     )
-    val_img_and_labels_df.rename(columns={'AbsPath': 'NpImage', 'Final Label Int': 'LabelTensor'}, inplace=True)
+    val_img_and_labels_df.rename(columns={'AbsPath': 'NpImage', 'Class Label': 'LabelTensor'}, inplace=True)
     val_ds = tf.data.Dataset.from_tensor_slices(
         (list(val_img_and_labels_df['NpImage']), list(val_img_and_labels_df['LabelTensor']))
     )
     del val_ds_df
     del val_img_and_labels_df
-    test_img_and_labels_df = test_ds_df[['AbsPath', 'Final Label Int']].apply(
+    test_img_and_labels_df = test_ds_df[['AbsPath', 'Class Label']].apply(
         load_and_preprocess_image, axis=1, raw=True, result_type='reduce', args=(
             ('color_mode', color_mode), ('target_size', target_size), ('interpolation', interpolation),
             ('keep_aspect_ratio', keep_aspect_ratio)
         )
     )
-    test_img_and_labels_df.rename(columns={'AbsPath': 'NpImage', 'Final Label Int': 'LabelTensor'}, inplace=True)
+    test_img_and_labels_df.rename(columns={'AbsPath': 'NpImage', 'Class Label': 'LabelTensor'}, inplace=True)
     test_ds = tf.data.Dataset.from_tensor_slices(
         (list(test_img_and_labels_df['NpImage']), list(test_img_and_labels_df['LabelTensor']))
     )
@@ -187,6 +200,6 @@ def load_datasets(
 if __name__ == '__main__':
     # Note: Change num_partitions to 1 to load in only Train_0, change to 2 to load in Train_0 and Train_1, etc.
     train_ds, val_ds, test_ds = load_datasets(
-        color_mode='rgb', target_size=(64, 64), interpolation='nearest', keep_aspect_ratio=False, num_partitions=6,
-        batch_size=32, num_images=None, train_set_size=0.6, val_set_size=0.2, test_set_size=0.2, seed=42
+        color_mode='rgb', target_size=(64, 64), interpolation='bilinear', keep_aspect_ratio=False, num_partitions=1,
+        batch_size=32, num_images=None, train_set_size=0.6, val_set_size=0.2, test_set_size=0.2, seed=42, is_multi=False
     )
