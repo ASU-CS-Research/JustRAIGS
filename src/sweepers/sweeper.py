cc8de744111577a33112be820779984bd154ae55
TayoOlofintuyi <tolofi152@gmail.com>
2024-04-09 11:27:19 -0400

    got some metadata about the labels set in dataset_analysis.py. Will try to see the correlation between labels

--- src/sweepers/sweeper.py
+++ src/sweepers/sweeper.py
@@ -42,7 +42,7 @@ def main():
     train_ds, val_ds, test_ds = load_datasets(
         color_mode='rgb', target_size=(75, 75), interpolation='bilinear', keep_aspect_ratio=False,
         train_set_size=0.6, val_set_size=0.2, test_set_size=0.2, seed=SEED, num_partitions=6, batch_size=BATCH_SIZE,
-        num_images=None, oversample_train_set=True, oversample_val_set=True, is_multi=False
+        num_images=900, oversample_train_set=True, oversample_val_set=True, is_multi=False
     )
     '''
     Initialize the WaB HyperModel in charge of setting up and executing individual trials as part of the sweep:
